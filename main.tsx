d6d019e76d3f3d15ef676ffcd8acac902bd55ae7
oliviereczeqss <oliviersuwala986@gmail.com>
2026-07-06 13:49:34 +0200

    Add files via upload

---
+++ main.tsx
@@ -0,0 +1,18 @@
+import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=291111af"; const jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport1_react from "/node_modules/.vite/deps/react.js?v=291111af"; const StrictMode = __vite__cjsImport1_react["StrictMode"];
+import __vite__cjsImport2_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=933a0f2a"; const createRoot = __vite__cjsImport2_reactDom_client["createRoot"];
+import App from "/src/App.tsx";
+import "/src/index.css";
+createRoot(document.getElementById("root")).render(
+  /* @__PURE__ */ jsxDEV(StrictMode, { children: /* @__PURE__ */ jsxDEV(App, {}, void 0, false, {
+    fileName: "/app/applet/src/main.tsx?__aistudio_auth_token=OzzCqPTyBlA7vxDDkS852McnmizGAeYwtZsIqA1Uoc0%3D",
+    lineNumber: 8,
+    columnNumber: 5
+  }, this) }, void 0, false, {
+    fileName: "/app/applet/src/main.tsx?__aistudio_auth_token=OzzCqPTyBlA7vxDDkS852McnmizGAeYwtZsIqA1Uoc0%3D",
+    lineNumber: 7,
+    columnNumber: 3
+  }, this)
+);
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4udHN4P19fYWlzdHVkaW9fYXV0aF90b2tlbj1PenpDcVBUeUJsQTd2eEREa1M4NTJNY25taXpHQWVZd3Rac0lxQTFVb2MwJTNEIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7U3RyaWN0TW9kZX0gZnJvbSAncmVhY3QnO1xuaW1wb3J0IHtjcmVhdGVSb290fSBmcm9tICdyZWFjdC1kb20vY2xpZW50JztcbmltcG9ydCBBcHAgZnJvbSAnLi9BcHAudHN4JztcbmltcG9ydCAnLi9pbmRleC5jc3MnO1xuXG5jcmVhdGVSb290KGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCdyb290JykhKS5yZW5kZXIoXG4gIDxTdHJpY3RNb2RlPlxuICAgIDxBcHAgLz5cbiAgPC9TdHJpY3RNb2RlPixcbik7XG4iXSwibWFwcGluZ3MiOiJBQU9JO0FBUEosU0FBUSxrQkFBaUI7QUFDekIsU0FBUSxrQkFBaUI7QUFDekIsT0FBTyxTQUFTO0FBQ2hCLE9BQU87QUFFUCxXQUFXLFNBQVMsZUFBZSxNQUFNLENBQUUsRUFBRTtBQUFBLEVBQzNDLHVCQUFDLGNBQ0MsaUNBQUMsU0FBRDtBQUFBO0FBQUE7QUFBQTtBQUFBLFNBQUssS0FEUDtBQUFBO0FBQUE7QUFBQTtBQUFBLFNBRUE7QUFDRjsiLCJuYW1lcyI6W119
